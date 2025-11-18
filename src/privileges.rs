180f8bcd64e24f6a49f8ccd67cb2f802cf5b3105
Vinitka's Dinasty <145179274+vinitkanov@users.noreply.github.com>
2025-11-18 15:05:31 +0700

    update TOML feature

--- src/privileges.rs
+++ src/privileges.rs
@@ -1,13 +1,14 @@
+use std::ptr;
 
 #[cfg(windows)]
 pub fn is_admin() -> bool {
+    use std::mem;
+    use std::ptr;
+    use winapi::um::handleapi::CloseHandle;
     use winapi::um::processthreadsapi::GetCurrentProcess;
+    use winapi::um::processthreadsapi::OpenProcessToken;
     use winapi::um::securitybaseapi::CheckTokenMembership;
     use winapi::um::winnt::{TokenElevation, TOKEN_ELEVATION, TOKEN_QUERY};
-    use winapi::um::handleapi::CloseHandle;
-    use winapi::um::processthreadsapi::OpenProcessToken;
-    use std::ptr;
-    use std::mem;
 
     let mut token_handle = ptr::null_mut();
     unsafe {
@@ -21,7 +22,12 @@ pub fn is_admin() -> bool {
     let elevation_size = mem::size_of::<TOKEN_ELEVATION>() as u32;
 
     unsafe {
-        if CheckTokenMembership(token_handle, &mut elevation as *mut _ as *mut _, &mut return_length) == 0 {
+        if CheckTokenMembership(
+            token_handle,
+            &mut elevation as *mut _ as *mut _,
+            &mut return_length,
+        ) == 0
+        {
             CloseHandle(token_handle);
             return false;
         }
@@ -37,25 +43,16 @@ pub fn is_admin() -> bool {
 #[cfg(windows)]
 pub fn relaunch_as_admin() -> anyhow::Result<()> {
     use std::env;
+    use std::ffi::OsStr;
+    use std::os::windows::ffi::OsStrExt;
     use std::process;
     use winapi::um::shellapi::ShellExecuteW;
     use winapi::um::winuser::SW_SHOW;
-    use std::ffi::OsStr;
-    use std::os::windows::ffi::OsStrExt;
 
     let exe = env::current_exe()?;
-    let exe_str: Vec<u16> = OsStr::new(&exe)
-        .encode_wide()
-        .chain(Some(0))
-        .collect();
-    let params_str: Vec<u16> = OsStr::new("")
-        .encode_wide()
-        .chain(Some(0))
-        .collect();
-    let verb_str: Vec<u16> = OsStr::new("runas")
-        .encode_wide()
-        .chain(Some(0))
-        .collect();
+    let exe_str: Vec<u16> = OsStr::new(&exe).encode_wide().chain(Some(0)).collect();
+    let params_str: Vec<u16> = OsStr::new("").encode_wide().chain(Some(0)).collect();
+    let verb_str: Vec<u16> = OsStr::new("runas").encode_wide().chain(Some(0)).collect();
 
     let result = unsafe {
         ShellExecuteW(
